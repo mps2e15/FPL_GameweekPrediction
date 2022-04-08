751aeddd4ad12fa7455e35d6473291f937eccfaf
“mps2e15” <mps2e15@soton.ac.uk>
2022-04-08 17:48:35 +0100

    Added LSTM custom layers

--- src/models/tf_model_utils.py
+++ src/models/tf_model_utils.py
@@ -33,3 +33,36 @@ class SequenceFlattener(tf.keras.layers.Layer):
             x = self.flattener(x)
             seq_list.append(x)
         return self.concatenate(seq_list)
+
+
+
+# %%
+class LSTM_Encoder(tf.keras.layers.Layer):
+    """LSTM Encoder"""
+
+    def __init__(self,seq_len=16, lstm_dim=32, dropout=0.2,name="LSTM_Encoder", **kwargs):
+        super(LSTM_Encoder, self).__init__(name=name, **kwargs)
+        self.static_repeater = tf.keras.layers.RepeatVector(seq_len)
+        self.concatenate_past = tf.keras.layers.Concatenate(name='concat_past')
+        self.lstm = tf.keras.layers.LSTM(lstm_dim, dropout=dropout,return_state=True,name='lstm')
+
+    def call(self, inputs):
+        x = self.static_repeater(inputs[0])
+        x = self.concatenate_past([x]+inputs[1:])
+        return self.lstm(x)
+
+class LSTM_Decoder(tf.keras.layers.Layer):
+    """LSTM Decoder"""
+
+    def __init__(self,seq_len=16, lstm_dim=32, dropout=0.2,name="LSTM_Decoder", **kwargs):
+        super(LSTM_Decoder, self).__init__(name=name, **kwargs)
+        self.state_repeater = tf.keras.layers.RepeatVector(seq_len)
+        self.concatenate_future = tf.keras.layers.Concatenate(name='concat_future')
+        self.lstm = tf.keras.layers.LSTM(lstm_dim, dropout=dropout,return_sequences=True,name='lstm')
+        self.out = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1))
+
+    def call(self, future_inputs,encoder_outputs):
+        x = self.state_repeater(encoder_outputs[0])
+        x = self.concatenate_future([x,future_inputs])
+        x = self.lstm(x,initial_state = encoder_outputs[1:])
+        return self.out(x)
